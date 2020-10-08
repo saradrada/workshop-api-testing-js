c48f5e400fa8f83aa33956b3ab77e45c0e64e318
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-10-08 13:05:32 -0500

    Static code verification

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -1,33 +1,33 @@
-const agent = require("superagent");
-const statusCode = require("http-status-codes");
-const chai = require("chai");
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const chai = require('chai');
 
-const expect = chai.expect;
+const { expect } = chai;
 
-describe("First Api Tests", () => {});
+describe('First Api Tests', () => {});
 
-it("Consume GET Service", async () => {
-  const response = await agent.get("https://httpbin.org/ip");
+it('Consume GET Service', async () => {
+  const response = await agent.get('https://httpbin.org/ip');
 
   expect(response.status).to.equal(statusCode.OK);
-  expect(response.body).to.have.property("origin");
+  expect(response.body).to.have.property('origin');
 });
 
-it("Consume GET Service with query parameters", async () => {
+it('Consume GET Service with query parameters', async () => {
   const query = {
-    name: "John",
-    age: "31",
-    city: "New York",
+    name: 'John',
+    age: '31',
+    city: 'New York'
   };
 
-  const response = await agent.get("https://httpbin.org/get").query(query);
+  const response = await agent.get('https://httpbin.org/get').query(query);
 
   expect(response.status).to.equal(statusCode.OK);
   expect(response.body.args).to.eql(query);
 });
 
-/*it("Consume PUT Service", async () => {
+/* it("Consume PUT Service", async () => {
   const response = await agent.get("https://httpbin.org/put");
 
   expect(response.status).to.equal(statusCode.OK);
-});*/
+}); */
