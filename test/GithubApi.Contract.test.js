b03f61aca18602be6a357973b20b6b9a16c84643
saradrada <sara.ortiz@correo.icesi.edu.co>
2020-12-16 14:12:41 -0500

    Scheme test

--- test/GithubApi.Contract.test.js
+++ test/GithubApi.Contract.test.js
@@ -1,20 +1,23 @@
-/* const chai = require('chai');
+const agent = require('superagent');
+const chai = require('chai');
 const { listPublicEventsSchema } = require('./schema/ListPublicEvents.schema');
 
 const { expect } = chai;
-chai.use(require('chai-json-schema')); */
-const request = require('./Request').instance;
+chai.use(require('chai-json-schema'));
+
+const urlBase = 'https://api.github.com';
 
 describe('Given event Github API resources', () => {
   describe('When wanna verify the List public events', () => {
     let response;
 
     before(async () => {
-      response = await request.get('events');
+      response = await agent
+        .get(`${urlBase}/events`)
+        .set('User-Agent', 'agent')
+        .auth('token', process.env.ACCESS_TOKEN);
     });
 
-    it.only('Then the body should have a schema', () => {
-      console.log(response);
-    });
+    it.only('then the body should have a schema', () => expect(response).to.be.jsonSchema(listPublicEventsSchema));
   });
 });
